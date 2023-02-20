e13eaca2a50cdc854df5e7fdb7f2463da000a903
Mohamad Mahdi Tasha <imwhdiiii@gmail.com>
2023-02-20 19:58:04 +0330

    yea nigg

--- src/routes/dashboard.js
+++ src/routes/dashboard.js
@@ -2,6 +2,7 @@
 import LeftSideComponent from '../components/left-side/left-side.js';
 import MainSideComponent from '../components/main-side/main-side.js';
 import RightSideComponent from '../components/right-side/right-side.js';
+import '../dist/style.css';
 
 export default function Dashboard() {
     return (
