cfcd22493b93312b216a4f50dde37e6a7a5670a6
Mohamad Mahdi Tasha <imwhdiiii@gmail.com>
2023-02-20 19:22:10 +0330

    yea again

--- src/App.js
+++ src/App.js
@@ -1,8 +1,3 @@
 // Codes By Mahdi Tasha
 import Dashboard from 'components/dashboard';
-
-export default function App() {
-    return (
-        <Dashboard />
-    )
-}
+export default function App() {return <Dashboard />}
