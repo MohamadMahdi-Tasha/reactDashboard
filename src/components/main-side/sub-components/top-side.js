3f633f4642de78a993e268154d5e9eedbb355037
Mohamad Mahdi Tasha <imwhdiiii@gmail.com>
2023-02-25 18:17:26 +0330

    added some file structures

--- src/components/main-side/sub-components/top-side.js
+++ src/components/main-side/sub-components/top-side.js
@@ -4,7 +4,7 @@ import MainNotifableButton from "./sub-components/main-notifable-button";
 
 export default function TopSideComponent() {
     return (
-        <div>
+        <div className='main-side__top'>
             <MainSearchComponent />
             <MainNotifableButton type={'massage'} notificationsCount={2}/>
             <MainNotifableButton type={'notifications'} notificationsCount={4}/>
