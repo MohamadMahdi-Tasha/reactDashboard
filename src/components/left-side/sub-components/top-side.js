e9633d450d145bc6ebe9685804b02b2baa0d79f3
Mohamad Mahdi Tasha <imwhdiiii@gmail.com>
2023-02-25 17:12:07 +0330

    added class names of left side (top components)

--- src/components/left-side/sub-components/top-side.js
+++ src/components/left-side/sub-components/top-side.js
@@ -4,16 +4,16 @@ import ThemeToggler from "./sub-components/left-theme-toggler";
 
 export default function TopSideComponent() {
     return (
-        <div>
-            <ul>
+        <div className='left-side__top'>
+            <ul className={'left-side__top-ul-1'}>
                 <LeftListItem title={'Dashboard'} active/>
                 <LeftListItem title={'Documents'} />
                 <LeftListItem title={'Payments'} />
                 <LeftListItem title={'Calendar'} />
                 <LeftListItem title={'Profile'} />
             </ul>
-            <hr/>
-            <ul>
+            <hr className={'left-side__top-divider'}/>
+            <ul className={'left-side__top-ul-2'}>
                 <ThemeToggler />
                 <LeftListItem title={'Setting'} />
                 <LeftListItem title={'Logout'} />
