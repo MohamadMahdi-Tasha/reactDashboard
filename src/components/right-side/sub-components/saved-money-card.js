68037dd5e73f9e09d841b866410f4de86fa6ff0b
Mohamad Mahdi Tasha <imwhdiiii@gmail.com>
2023-02-26 19:17:58 +0330

    added class names for bottom side of right side component

--- src/components/right-side/sub-components/saved-money-card.js
+++ src/components/right-side/sub-components/saved-money-card.js
@@ -1,12 +1,12 @@
 // Codes By Mahdi Tasha
 export default function SavedMoneyCard(props) {
     return (
-        <div>
-            <div>
-                <h6>Saved Money</h6>
-                <h6><span></span>Your Card</h6>
+        <div className='right-side__money'>
+            <div className='right-side__money-content'>
+                <h6 className='right-side__money-title'>Saved Money</h6>
+                <h6 className='right-side__money-subtitle'>Your Card</h6>
             </div>
-            <h6>${props.value.toLocaleString()}</h6>
+            <h6 className='right-side__money-price'>${props.value.toLocaleString()}</h6>
         </div>
     );
 }
