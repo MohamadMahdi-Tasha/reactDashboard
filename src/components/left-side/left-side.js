6268c5a1821dc3536851608271f4e9860ea10aad
Mohamad Mahdi Tasha <imwhdiiii@gmail.com>
2023-02-27 16:25:09 +0330

    added written scripts of last commit to its parent component

--- src/components/left-side/left-side.js
+++ src/components/left-side/left-side.js
@@ -3,12 +3,10 @@ import React, { useState} from 'react';
 import TopSideComponent from './sub-components/top-side'
 import BottomSideComponent from './sub-components/bottom-side';
 
-export default function LeftSideComponent() {
-    const [isOpened, setOpenOrClose] = useState(true);
-
+export default function LeftSideComponent(props) {
     return (
-        <div className='left-side' data-opened={isOpened}>
-            <TopSideComponent onCloseButtonClick={() => setOpenOrClose(false)}/>
+        <div className='left-side' data-opened={props.isOpened}>
+            <TopSideComponent onCloseButtonClick={props.onCloseButtonClick}/>
             <BottomSideComponent />
         </div>
     );
--- src/routes/dashboard.js
+++ src/routes/dashboard.js
@@ -1,13 +1,16 @@
 // Codes By Mahdi Tasha
+import React, { useState } from "react";
 import '../dist/style.css';
 import LeftSideComponent from '../components/left-side/left-side.js';
 import MainSideComponent from '../components/main-side/main-side.js';
 import RightSideComponent from '../components/right-side/right-side.js';
 
 export default function Dashboard() {
+    const [isOpened, setOpenOrClose] = useState(true);
+
     return (
         <div className='dashboard'>
-            <LeftSideComponent />
+            <LeftSideComponent isOpened={isOpened} onCloseButtonClick={() => setOpenOrClose(false)}/>
             <MainSideComponent />
             <RightSideComponent />
         </div>
